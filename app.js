858994a81b66270a27a04d571de822d7cd1c7e71
kiabetsu <srcback@yandex.ru>
2025-08-21 22:46:51 +0300

    8-crypto3

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const crypto = (string) => {
   let stringArray = string.split('');
   let firstPart = stringArray.slice(0, stringArray.length / 2).reverse();
-  let secondPart = stringArray.slice(stringArray.length / 2).reverse();
+  let secondPart = stringArray.slice(stringArray.length / 2);
+  const firstLet = secondPart.at(0);
+  const lastLet = secondPart.at(-1);
+  secondPart.shift();
+  secondPart.unshift(lastLet);
+  secondPart.pop();
+  secondPart.push(firstLet);
 
   const res = firstPart.join('') + secondPart.join('');
   return res;
@@ -11,7 +17,8 @@ console.log(crypto('password'));
 
 const check = (string, check) => {
   const res = crypto(string);
+  console.log(res);
   return res === check;
 };
 
-console.log(check('ssapdrow', 'wrong'));
+console.log(check('ssapdorw', 'password'));
