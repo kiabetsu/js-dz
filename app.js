6b3a5cfa7a6b151dfcb22e2d900daaa50b32cc3d
kiabetsu <srcback@yandex.ru>
2025-08-25 02:42:26 +0300

    12-luna2

--- app.js
+++ app.js
@@ -2,20 +2,21 @@ const number = '4561-2612-1534-5464';
 
 const luna = (number) => {
   number = number.replace(/-/g, '');
-  if (!number.length === 16) {
+  if (number.length !== 16) {
     return false;
   }
   number = number.split('');
+  number = number.reverse();
   number = number
     .map((value, i) => {
-      if (i % 2 === 0) {
+      if (i % 2 === 1) {
         return value * 2;
       } else {
         return value;
       }
     })
     .map((value, i) => {
-      if (i % 2 === 0) {
+      if (i % 2 === 1) {
         if (value > 9) {
           return value - 9;
         } else {
@@ -25,10 +26,7 @@ const luna = (number) => {
       return value;
     });
 
-  console.log(number);
-
   const sumNumber = number.reduce((acc, value) => (acc += Number(value)), 0);
-  console.log(sumNumber);
 
   if (sumNumber % 10 === 0) {
     return true;
