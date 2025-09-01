1382c092c040b080228fd01b92a279e537dbec02
kiabetsu <srcback@yandex.ru>
2025-09-01 23:16:01 +0300

    1-map3

--- app.js
+++ app.js
@@ -4,14 +4,8 @@ const arr = [
   { id: 1, name: 'Вася' },
 ];
 
-const seen = new Set();
-const newArr = arr.filter((el) => {
-  if (seen.has(el.id)) {
-    return false;
-  } else {
-    seen.add(el.id);
-    return true;
-  }
-});
+const uniqKeys = new Set(arr.map((el) => el.id));
+console.log(uniqKeys);
+const newArr = [...uniqKeys].map((key) => arr.find((element) => element.id === key));
 
 console.log(newArr);
