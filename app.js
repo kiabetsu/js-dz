8c24ffb5076afeea12668e06fd6a6202ee35252e
kiabetsu <srcback@yandex.ru>
2025-09-02 23:29:53 +0300

    2-dice3

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-function throwD(number) {
+function throwD(dice) {
+  dice = Number(dice.replace('d', ''));
   const correctNumb = [4, 6, 8, 10, 12, 16, 20];
-  if (correctNumb.includes(number)) {
-    return Math.trunc(Math.random() * number + 1);
+  console.log(correctNumb.includes(dice));
+  if (correctNumb.includes(dice)) {
+    return Math.trunc(Math.random() * dice + 1);
   } else {
     console.log('incorrect number to throw');
     return null;
   }
 }
 
-console.log(throwD(20));
+console.log(throwD('d20'));
