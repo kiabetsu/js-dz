51502847a25418bee64cb932fd77eb7a5c95ebec
kiabetsu <srcback@yandex.ru>
2025-09-09 22:02:16 +0300

    8-solid

---
+++ app.js
@@ -0,0 +1,20 @@
+class Billing {
+  amount = 0;
+
+  calculateTotal(prices) {
+    this.amount = prices.reduce(acc, (value) => (acc += value));
+  }
+
+  fixBilling(prices) {
+    this.calculateTotal(prices);
+    return this.amount;
+  }
+
+  hourBilling(prices, hours) {
+    return this.fixBilling(prices) * hours;
+  }
+
+  itemBilling(prices, count) {
+    return this.fixBilling(prices) * items;
+  }
+}
