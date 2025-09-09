b8ef6636b2f24774597691d94487a89d06065ad3
kiabetsu <srcback@yandex.ru>
2025-09-09 22:09:04 +0300

    8-solid3

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ class Billing {
   calculateTotal(prices) {
     this.amount = prices.reduce((acc, value) => (acc += value), 0);
   }
+}
 
+class FixBilling extends Billing {
   fixBilling(prices) {
     this.calculateTotal(prices);
     return this.amount;
   }
+}
 
+class HourBilling extends FixBilling {
   hourBilling(prices, hours) {
     return this.fixBilling(prices) * hours;
   }
+}
 
+class ItemBilling extends FixBilling {
   itemBilling(prices, count) {
     return this.fixBilling(prices) * count;
   }
