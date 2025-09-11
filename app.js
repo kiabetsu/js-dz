e4714ccac93cd989af7dadec6afd6d7d2fa96549
kiabetsu <srcback@yandex.ru>
2025-09-11 21:44:00 +0300

    10-2

--- app.js
+++ app.js
@@ -10,11 +10,12 @@ const getData = (url, errorMessage) => {
 };
 
 const getAbility = () => {
-  getData('https://pokeapi.co/api/v2/pokemon/ditto', 'can not receive pokemon')
-    .then((data) => getData(data.abilities[0].ability.url, 'cant receive ability'))
+  getData('https://pokeapi.co/api/v2/pokemon/ditto', 'cant find pokemon')
+    .then((data) => getData(data.abilities[0].ability.url, 'cant find ability'))
     .then((data) =>
       console.log(data.flavor_text_entries.find((lang) => lang.language.name === 'en')),
-    );
+    )
+    .catch((error) => console.log('cant find ability language', error));
 };
 
 getAbility();
