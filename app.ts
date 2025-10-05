ef88aac4f8add1c16ee264d7c8f8bf6d28e49843
kiabetsu <srcback@yandex.ru>
2025-10-05 23:27:06 +0300

    8-pick

--- app.ts
+++ app.ts
@@ -1,1 +1,20 @@
-const a = 5;
+const user = {
+  name: 'Vasiliy',
+  age: 8,
+  skills: ['typescript', 'javascript'],
+};
+
+function pickObjectKeys<T extends object, K extends keyof T>(
+  obj: T,
+  properties: K[],
+): Record<K, any> {
+  let res: Record<K, any>;
+  res = {} as Record<K, any>;
+  for (let property of properties) {
+    if (property in obj) {
+      res[property] = obj[property];
+    }
+  }
+  return res;
+}
+console.log(pickObjectKeys(user, ['age', 'skills']));
